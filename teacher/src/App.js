a21a9bd5e3f56429abc86a7015f8d2659773961d
ghazi hadded <ghazihadded6@gmail.com>
2021-01-13 15:26:29 +0100

    login

--- teacher/src/App.js
+++ teacher/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {Provider} from 'react-redux'
 import {User} from "./components/User/User"
 import {AccountSettings} from './components/AccountSettings/AccountSettings'
 import {HomeApp} from './components/home/HomeApp'
 import NavBar from './components/navbar/NavBar'
+import LoginPage from './components/LoginPage'
+import store from './js/Store'
 
 function App() {
+
+
   return (
-    <Router>
-        <NavBar/>
+  
+    <Provider store={store}>
+     <Router>
+        
+        <Route path="/" exact component={LoginPage} />  
+        <Route path="/profile"  component={NavBar} /> 
+      
       <Switch>
-      <Route path="/" exact component={HomeApp} />
+        
+        
+         <Route path="/profile" exact component={HomeApp} />
         <Route path="/user" component={User} />
-        <Route path="/settings" component={AccountSettings} />
+        <Route path="/settings" component={AccountSettings} /> 
         
       </Switch>
-    </Router>
+      
+    </Router> 
+
+    </Provider>
+    
   );
 }
 
